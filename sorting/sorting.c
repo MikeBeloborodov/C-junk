b5fbc76e1d6bdf8f0619900dc73fe92aac33881b
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-06-24 20:45:10 +0400

    added merge sort

--- sorting/sorting.c
+++ sorting/sorting.c
@@ -2,7 +2,62 @@
 #include <stdlib.h>
 
 int qs_times_sorted = 0;
+int ms_times_sorted = 0;
 int times_sorted = 0;
+
+void merge_sorted_arrays(int array[], int left, int middle, int right)
+{
+    int left_length = middle - left + 1;
+    int right_length = right - middle;
+
+    int temp_left[left_length];
+    int temp_right[right_length];
+
+    int i, j, k;
+
+    for (int i = 0; i < left_length; i++)
+    {
+        temp_left[i] = array[left + i];
+    }
+    for (int i = 0; i < right_length; i++)
+    {
+        temp_right[i] = array[middle + 1 + i];
+    }
+
+    for (i = 0, j = 0, k = left; k <= right; k++)
+    {
+        ms_times_sorted++;
+        if ((i < left_length) && 
+                (j >= right_length || temp_left[i] <= temp_right[j]))
+        {
+            array[k] = temp_left[i];
+            i++;
+        }
+        else
+        {
+            array[k] = temp_right[j];
+            j++;
+        }
+
+    }
+}
+
+void merge_sort_recursive(int array[], int left, int right)
+{
+    if (left < right)
+    {
+        int middle = left + (right - left) / 2;
+        merge_sort_recursive(array, left, middle);
+        merge_sort_recursive(array, middle + 1, right);
+
+        merge_sorted_arrays(array, left, middle, right);
+    }
+}
+
+void merge_sort(int array[], int length)
+{
+    merge_sort_recursive(array, 0, length - 1);
+}
 
 void fill_arr_with_rand_nums(int arr[], int length)
 {
@@ -26,11 +81,11 @@ int partition(int array[], int low, int high)
 
     for (int j = low; j < high; j++)
     {
+        qs_times_sorted++;
         if (array[j] <= pivot_value)
         {
             swap(&array[j], &array[i]);
             i++;
-            qs_times_sorted++;
         }
     }
 
@@ -67,11 +122,11 @@ int insertion_sort(int arr[], int length)
         minValue = arr[i];
         for (int j = i + 1; j < length; j++)
         {
+            times_sorted++;
             if (arr[j] < minValue)
             {
                indexMinValue = j;
                minValue = arr[j];
-               times_sorted++;
             }
         }
         int buffer = arr[i];
@@ -92,13 +147,13 @@ int bubble_sort(int arr[], int length)
         sorted = 0;
         for (int i = 0; i < length - 1; i++)
         {
+           times_sorted++;
            if (arr[i] > arr[i + 1])
            {
                int buffer = arr[i];
                arr[i] = arr[i + 1];
                arr[i + 1] = buffer;
                sorted = 1;
-               times_sorted++;
            }
         }
     }
@@ -120,34 +175,48 @@ int main()
     int arr [] = {3, 4, 2, 1, 5, 10, 7, 3, 11, 0};
     int arr2 [] = {4, 2, 1, 3, 11, 0, 6, 5, 7, 20};
     int arr3 [] = {1, 3, 10, 2, 4, 5, 8, 6, 20, 31};
+    int arr4 [] = {4, 2, 6, 9, 1, 0, 10, 5, 3, 41};
 
-   //// bubble sort
-   //times_sorted = bubble_sort(arr, length);
-   //printf("Bubble sort: \n");
-   //print_array(arr, length);
-   //printf("\nTimes sorted: %d", times_sorted);
+    // small numbers
+    printf("---------Small numbers----------\n");
+    // bubble sort
+    times_sorted = bubble_sort(arr, length);
+    printf("Bubble sort: \n");
+    print_array(arr, length);
+    printf("\nTimes sorted: %d", times_sorted);
 
-   //// insertion sort
-   //times_sorted = insertion_sort(arr2, length);
-   //printf("\nInsertion sort: \n");
-   //print_array(arr2, length);
-   //printf("\nTimes sorted: %d", times_sorted);
+    // insertion sort
+    times_sorted = insertion_sort(arr2, length);
+    printf("\nInsertion sort: \n");
+    print_array(arr2, length);
+    printf("\nTimes sorted: %d", times_sorted);
 
-   //// quick sort
-   //printf("\nQuick sort: \n");
-   //quick_sort(arr3, length);
-   //print_array(arr3, length);
-   //printf("\nTimes sorted: %d", qs_times_sorted);
+    // quick sort
+    printf("\nQuick sort: \n");
+    quick_sort(arr3, length);
+    print_array(arr3, length);
+    printf("\nTimes sorted: %d", qs_times_sorted);
+
+    // merge sort
+    printf("\nMerge sort: \n");
+    merge_sort(arr4, length);
+    print_array(arr4, length);
+    printf("\nTimes sorted: %d", ms_times_sorted);
 
     // big guns :)
     int big_length = 20000;
     int arr1_big[big_length];
     int arr2_big[big_length];
     int arr3_big[big_length];
+    int arr4_big[big_length];
 
     fill_arr_with_rand_nums(arr1_big, big_length);
     fill_arr_with_rand_nums(arr2_big, big_length);
     fill_arr_with_rand_nums(arr3_big, big_length);
+    fill_arr_with_rand_nums(arr4_big, big_length);
+
+    // big numbers
+    printf("\n---------Big numbers----------\n");
 
     // bubble sort
     times_sorted = bubble_sort(arr1_big, big_length);
@@ -158,8 +227,14 @@ int main()
     printf("Insertion sort times: %d\n", times_sorted);
 
     // quick sort
+    qs_times_sorted = 0;
     quick_sort(arr3_big, big_length);
     printf("Quick sort times: %d\n", qs_times_sorted);
+
+    // merge_sort
+    ms_times_sorted = 0;
+    merge_sort(arr4_big, big_length);
+    printf("Merge sort times: %d\n", ms_times_sorted);
     
     return 0;
 }
